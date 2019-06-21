3828b545856d81bb5ae3ba82f509eef5d33afd81
Alvin <alvin.christian7@yahoo.com>
2019-06-21 10:01:55 +0700

    my gifs on progress

---
+++ client/src/components/Landing.vue
@@ -0,0 +1,200 @@
+<template>
+  <div>
+    <!--Modal: Login / Register Form-->
+    <div
+      id="get-started"
+      class="text-center"
+      style="font-family: 'Bungee Shade', cursive;background: transparent;"
+    >
+      <p>Let's have some fun, shall we?</p>
+      <a
+        href="#"
+        class="btn btn-default btn-rounded my-3"
+        data-toggle="modal"
+        data-target="#modalLRForm"
+        style="font-family: 'Eater', cursive;"
+      >GIF ME NOW</a>
+    </div>
+    <!--Modal: Login / Register Form-->
+    <div
+      class="modal fade"
+      id="modalLRForm"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="myModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog cascading-modal" role="document">
+        <!--Content-->
+        <div class="modal-content">
+          <!--Modal cascading tabs-->
+          <div class="modal-c-tabs">
+            <!-- Nav tabs -->
+            <ul id="authTab" class="nav nav-tabs md-tabs tabs-2 light-blue darken-3" role="tablist">
+              <li class="nav-item">
+                <a class="nav-link active" data-toggle="tab" href="#panel7" role="tab">
+                  <i class="fas fa-user mr-1"></i>
+                  Login
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#panel8" role="tab">
+                  <i class="fas fa-user-plus mr-1"></i>
+                  Register
+                </a>
+              </li>
+            </ul>
+
+            <!-- Tab panels -->
+            <div class="tab-content">
+              <!--Panel 7-->
+              <div class="tab-pane fade in show active" id="panel7" role="tabpanel">
+                <!--Body-->
+                <div class="modal-body mb-1">
+                  <form @submit.prevent="login">
+                    <div class="md-form form-sm mb-5">
+                      <i class="fas fa-envelope prefix"></i>
+                      <input
+                        type="email"
+                        id="modalLRInput10"
+                        class="form-control form-control-sm validate"
+                      >
+                      <label data-error="wrong" data-success="right" for="modalLRInput10">Your email</label>
+                    </div>
+  
+                    <div class="md-form form-sm mb-4">
+                      <i class="fas fa-lock prefix"></i>
+                      <input
+                        type="password"
+                        id="modalLRInput11"
+                        class="form-control form-control-sm validate"
+                      >
+                      <label
+                        data-error="wrong"
+                        data-success="right"
+                        for="modalLRInput11"
+                      >Your password</label>
+                    </div>
+                    <div class="row justify-content-around align-items-center mt-2">
+                      <button class="btn btn-info">
+                        Log in
+                        <i class="fas fa-sign-in ml-1"></i>
+                      </button>
+                      <div class="g-signin2" data-onsuccess="onSignIn"></div>
+                    </div>
+                  </form>
+                </div>
+                <!--Footer-->
+                <div class="modal-footer">
+                  <div class="options text-center text-md-right mt-1">
+                    <p>
+                      Not a member?
+                      <a
+                        onclick="$('#authTab li:last-child a').tab('show')"
+                        href="#"
+                        class="blue-text"
+                      >Sign Up</a>
+                    </p>
+                    <!-- <p>
+                          Forgot
+                          <a href="#" class="blue-text">Password?</a>
+                    </p>-->
+                  </div>
+                  <button
+                    type="button"
+                    class="btn btn-outline-info waves-effect ml-auto"
+                    data-dismiss="modal"
+                  >Close</button>
+                </div>
+              </div>
+              <!--/.Panel 7-->
+
+              <!--Panel 8-->
+              <div class="tab-pane fade" id="panel8" role="tabpanel">
+                <!--Body-->
+                <div class="modal-body">
+                  <form @submit.prevent="register">
+                    <div class="md-form form-sm mb-5">
+                      <i class="fas fa-envelope prefix"></i>
+                      <input
+                        type="email"
+                        id="modalLRInput12"
+                        class="form-control form-control-sm validate"
+                      >
+                      <label data-error="wrong" data-success="right" for="modalLRInput12">Your email</label>
+                    </div>
+  
+                    <div class="md-form form-sm mb-5">
+                      <i class="fas fa-lock prefix"></i>
+                      <input
+                        type="password"
+                        id="modalLRInput13"
+                        class="form-control form-control-sm validate"
+                      >
+                      <label
+                        data-error="wrong"
+                        data-success="right"
+                        for="modalLRInput13"
+                      >Your password</label>
+                    </div>
+  
+                    <div class="md-form form-sm mb-4">
+                      <i class="fas fa-lock prefix"></i>
+                      <input
+                        type="password"
+                        id="modalLRInput14"
+                        class="form-control form-control-sm validate"
+                      >
+                      <label
+                        data-error="wrong"
+                        data-success="right"
+                        for="modalLRInput14"
+                      >Repeat password</label>
+                    </div>
+                    <div class="custom-file">
+                      <input type="file" class="custom-file-input" id="customFile">
+                      <label class="custom-file-label" for="customFile">Choose file</label>
+                    </div>
+                    <div class="text-center form-sm mt-2">
+                      <button class="btn btn-info">
+                        Sign up
+                        <i class="fas fa-sign-in ml-1"></i>
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <!--Footer-->
+                <div class="modal-footer">
+                  <div class="options text-right">
+                    <p class="pt-1">
+                      Already have an account?
+                      <a
+                        onclick="$('#authTab li:first-child a').tab('show')"
+                        href="#"
+                        class="blue-text"
+                      >Log In</a>
+                    </p>
+                  </div>
+                  <button
+                    type="button"
+                    class="btn btn-outline-info waves-effect ml-auto"
+                    data-dismiss="modal"
+                  >Close</button>
+                </div>
+              </div>
+              <!--/.Panel 8-->
+            </div>
+          </div>
+        </div>
+        <!--/.Content-->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import 'bootstrap';
+// // import 'mdbootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'jquery';
+// import 'popper.js';
+// import '@fortawesome/fontawesome-free';
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
