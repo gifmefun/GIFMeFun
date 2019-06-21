358cf979d2d576ee7a5e03aadb1a7ab3fb1e1260
fadlulazmi <fadlul_azmi@yahoo.com>
2019-06-21 15:02:37 +0700

    implement vue

--- client/src/components/EditProfile.vue
+++ client/src/components/EditProfile.vue
@@ -6,11 +6,11 @@
       <!-- left column -->
       <div class="col-md-3">
         <div class="text-center">
-          <img src="//placehold.it/100" class="avatar img-circle" alt="avatar">
+          <img :src="imageURL" class="avatar img-circle" alt="avatar">
           <h6>Upload a different photo...</h6>
           
           <div class="custom-file">
-                      <input type="file" class="custom-file-input" id="customFile">
+                      <input @change="selectFile" type="file" class="custom-file-input" id="customFile">
                       <label class="custom-file-label" for="customFile">Choose file</label>
                     </div>
         </div>
@@ -18,11 +18,11 @@
       
       <!-- edit form column -->
       <div class="col-md-9 personal-info">
-        <div class="alert alert-info alert-dismissable">
+        <!-- <div class="alert alert-info alert-dismissable">
           <a class="panel-close close" data-dismiss="alert">×</a> 
           <i class="fa fa-coffee"></i>
           This is an <strong>.alert</strong>. Use this to show important messages to the user.
-        </div>
+        </div> -->
         <h3>Personal info</h3>
         
         <form class="form-horizontal" role="form">
@@ -47,7 +47,7 @@
           <div class="form-group">
             <label class="col-md-3 control-label"></label>
             <div class="col-md-8">
-              <input type="button" class="btn btn-primary" value="Save Changes">
+              <input @click="updateProfile" type="button" class="btn btn-primary" value="Save Changes">
               <span></span>
               <input type="reset" class="btn btn-default" value="Cancel">
             </div>
@@ -57,3 +57,47 @@
   </div>
 </div>
 </template>
+<script>
+import axios from '../axios.js'
+export default {
+  data(){
+    return {
+      name: localStorage.getItem('name'),
+      email: localStorage.getItem('email'),
+      imageURL: localStorage.getItem('imageURL'),
+      image: ''
+    }
+  },
+  methods: {
+    selectFile() {
+      if(event.target.files[0])
+        this.image = event.target.files[0];
+    },
+    updateProfile() {
+      let formData = new FormData()
+        formData.append('name',this.name)
+        formData.append('email',this.email)
+        formData.append('password',this.password)
+        formData.append('imageURL',this.image)
+      axios({
+        method: "put",
+        url: `/user`,
+        data: formData
+      })
+        .then(({ data }) => {
+      this.password = "";          
+          Swal.fire({
+            type: "success",
+            title: "Registered!",
+            showConfirmButton: false,
+            timer: 2000
+          });
+        })
+        .catch(({ response }) => {
+      this.password = "";
+          Swal.fire("Error", response.data.message, "error");
+        });
+    },
+  }
+}
+</script>
